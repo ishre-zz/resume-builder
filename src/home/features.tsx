fc7925c2da08ebed8692d8b5699914e8edb4b80b
SHREYASH DUBEY <askshreyash@gmail.com>
2022-06-14 17:25:56 +0530

    Update features.tsx

--- src/home/features.tsx
+++ src/home/features.tsx
@@ -41,27 +41,18 @@ const Features: NextPage = () => {
       <div>
         <Image src="/home/no-account.png" alt="No account" height="50px" width="50px" />
         <h2>No signup/signin</h2>
-        <p>
-          Completely free and open source. No registration required. We do not store any of your
-          information. Create unlimited resumes using our tool.
-        </p>
+       
       </div>
       <div>
         <Image src="/home/timer.png" alt="No account" height="50px" width="50px" />
 
         <h2>Build in minutes</h2>
-        <p>
-          With the powerful editor and wide range of professinal/job ready templates, you can build
-          your resume within a matter few clicks & key strokes.
-        </p>
+        
       </div>
       <div>
         <Image src="/home/resume.png" alt="No account" height="50px" width="50px" />
         <h2>Fit for your needs</h2>
-        <p>
-          If you are a developer, customize the template which suits your data and all you need to
-          know is some basic knowledge of React and programming.
-        </p>
+        
       </div>
     </FeatureContainer>
   );
